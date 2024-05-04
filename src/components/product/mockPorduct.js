ae9dad22734def1155c8c78d84ff002aa638c1c5
VRAJ9R5 <rahul.raj2@pearson.com>
2024-05-04 22:50:44 +0530

    creating product card

---
+++ src/components/product/mockPorduct.js
@@ -0,0 +1,22 @@
+const mockProducts = [
+  {
+    id: 1,
+    name: "Product 1",
+    image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_640.jpg",
+    rating: 4,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    image: "https://via.placeholder.com/150",
+    rating: 3.5,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    image: "https://via.placeholder.com/150",
+    rating: 5,
+  },
+];
+
+export default mockProducts;
